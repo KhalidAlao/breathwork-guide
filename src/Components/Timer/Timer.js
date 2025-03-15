800649b3e03b093c3f60b321ae666344725de673
KhalidAlao <alaokha@gmail.com>
2025-03-15 07:45:05 +0000

    Update Timer.js

--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -2,12 +2,13 @@ import React from 'react';
 import React from './Timer.css'
 
 
-
-
-
-
-
-
-
+const Timer = ({ timeLeft, phase }) => {
+    return (
+      <div style={{ margin: '1rem' }}>
+        <h2>{phase.charAt(0).toUpperCase() + phase.slice(1)} Time: {timeLeft}s</h2>
+      </div>
+    );
+  };
+  
 
 export default Timer;
